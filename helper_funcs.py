7ed5e4a980280546cd9252f41d24c075eaee6572
Johannes Steinmetzer <josteinm@posteo.de>
2016-10-11 11:37:08 +0200

    helper_funcs.py: Further deletions.

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os.path
-import re
-
-import numpy as np
-from mytabulate import tabulate
-
-def hartree2eV(hartree):
-    return hartree * 27.2114
-
-def hartree2nm(hartree):
-    return 6.626-34 * 2.9979e8 * 1e9 / (hartree * 27.2114 * 1.6e-19)
-
-def nm2eV(nm):
-    return 6.626e-34 / 1.6021e-19 * 2.9979e8 / (nm / 1.e9)
-
 def eVformat(eVs):
     fmt_str = "{:.2f}"
     return [fmt_str.format(eV) for eV in eVs]
